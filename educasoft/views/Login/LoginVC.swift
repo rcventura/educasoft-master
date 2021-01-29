33617ca35f92772eab8bf842d67e57d9eb428384
Rodrigo Ventura <rcvanalista@gmail.com>
2021-01-29 15:36:53 -0300

    Tela Login
    Criação da tela de login com Firebase

--- educasoft/views/Login/LoginVC.swift
+++ educasoft/views/Login/LoginVC.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var bntLogin: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        self.bntLogin.layer.cornerRadius = 10
+        
+    }
+    @IBAction func bntLoginAC(_ sender: UIButton) {
+        if let email = self.userTextField.text, let password = self.passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) {
+                authResult, error in
+                
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "LoginToApp", sender: self)
+                }
+            }
+        }
     }
 }
---
+++ educasoft/views/Login/cells/LoginTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LoginTableViewCell.swift
+//  educasoft
+//
+//  Created by Rodrigo Ventura on 28/01/21.
+//
+
+import UIKit
+
+class LoginTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
